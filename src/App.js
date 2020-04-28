65c753920e605ee77e9bc37a2fcb7b8dedf01ee3
unknown <cacup7@gmail.com>
2020-04-28 19:12:18 -0300

    Componente header-comp criado.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import HeaderComp from './components/header-comp/index'
 
 // import { Container } from './styles';
 
 export const App = () => 
 <div>
-  coiso aqui;
+  <HeaderComp />
 </div>;
 
 export default App;
---
+++ src/components/header-comp/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+
+const HeaderComp = () => 
+  <div id="header-comp">
+    Header Aqui
+  </div>;
+
+export default HeaderComp;
